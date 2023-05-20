0a013b0660fa33d704469d46bcb4f91467874101
ashi12001 <ashishgurjar12001@gmail.com>
2023-05-20 07:37:02 +0530

    Renamed

---
+++ java/11Strings/_Strings.java
@@ -0,0 +1,10 @@
+public class _Strings {
+                                        
+       public static void main(String[] args) {
+        String name ="ashi";
+        System.out.println(name);
+                                        
+                                        
+                                        
+  }
+}
