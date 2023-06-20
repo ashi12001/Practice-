d05c4663131e34a135062830abf17065cc41ebb6
ashi12001 <ashishgurjar12001@gmail.com>
2023-06-20 15:24:39 +0530

    initial

--- java/8.3 oops/_2AM.java
+++ java/8.3 oops/_2AM.java
@@ -5,6 +5,10 @@ public class _2AM {
         Animals obj =new Animals();
         obj.run();
         obj.Species = "lion";
+        obj.breed ="lionera";
+        obj.colour= "palpal";
+
+        System.out.println(obj.colour);
 
         System.out.println(obj.Species);
 
